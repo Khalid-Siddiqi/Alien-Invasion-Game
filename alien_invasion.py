3d90bdd05ba6c2931c5b46e74f1eea08672669b2
Khalid Khurshid Siddiqui <khalid20031016@gmail.com>
2026-02-12 16:56:18 +0500

    added settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,14 @@
 import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
         self.clock = pygame.time.Clock()
-        self.bg_color = (200,200,200)
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         ## pygame.display.set_mode is a surface, this particular surface is where game elements will be displayed
         pygame.display.set_caption("Alien Invasion")
 
@@ -20,7 +20,8 @@ class AlienInvasion:
                 # when player clicks on close button this event is generated and sys.exit() is called to close the window
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            #fill background with this colour
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
             self.clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A Class to store all settings for for Alien Invasion"""
+    def __init__(self):
+        """Initialize the games's settings."""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
