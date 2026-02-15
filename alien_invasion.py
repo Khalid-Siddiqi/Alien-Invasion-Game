6d1e2a73592d1318402dd2c1a517d5b92f8fd3f7
Khalid Khurshid Siddiqui <khalid20031016@gmail.com>
2026-02-15 21:16:32 +0500

    updated shi[

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
     def __init__(self):
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         ## pygame.display.set_mode is a surface, this particular surface is where game elements will be displayed
         pygame.display.set_caption("Alien Invasion")
+        self.ship= Ship(self)
 
     def run_game(self):
         """Starting the main loop for the game"""
@@ -22,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
             #fill background with this colour
             self.screen.fill(self.settings.bg_color)
-
+            self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
 
